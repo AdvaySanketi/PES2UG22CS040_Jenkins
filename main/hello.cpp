bf82e5fd7be73cd9cefdadeb8f58b7f77138d29e
Advay Sanketi <advay2807@gmail.com>
2025-03-10 16:23:34 +0530

    One more commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "PES2UG22CS040 \n";
+  cout << "SRN: PES2UG22CS040 \n";
   return 0;
 }
